2a12188ff856d138ccf4d582d2c5b69b2794a38a
giorgio <giorgio.programmer@gmail.com>
2023-05-29 15:55:42 +0200

    commit specials to main

--- frontend/index.ts
+++ frontend/index.ts
@@ -2,4 +2,8 @@ import { createElement } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.js';
 
+import { CounterEndpoint } from './generated/endpoints.js';
+
 createRoot(document.getElementById('outlet')!).render(createElement(App));
+
+CounterEndpoint.addOne(1).then((result) => console.log(result));
